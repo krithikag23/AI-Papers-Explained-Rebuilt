322c75eb7e1f48972c014a4cc302e7a2c3693e96
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-01-30 13:10:22 +0530

    Update positional_encoding.py

--- papers/01_Attention_Is_All_You_Need/positional_encoding.py
+++ papers/01_Attention_Is_All_You_Need/positional_encoding.py
@@ -1,1 +1,6 @@
+"""
+Sinusoidal Positional Encoding (from 'Attention Is All You Need').
 
+This file implements fixed positional encodings using sine/cosine waves,
+so Transformers can capture token order without recurrence.
+"""
