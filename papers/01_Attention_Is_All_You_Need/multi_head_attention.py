08934399a4898bf163d96cd22df2586e30606fec
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-02-02 22:00:03 +0530

    Update multi_head_attention.py

--- papers/01_Attention_Is_All_You_Need/multi_head_attention.py
+++ papers/01_Attention_Is_All_You_Need/multi_head_attention.py
@@ -1,1 +1,7 @@
+"""
+Multi-Head Attention (from 'Attention Is All You Need') implemented in NumPy.
+
+This file builds on scaled dot-product attention and shows how multiple heads
+can attend to different subspaces of the representation.
+"""
 
