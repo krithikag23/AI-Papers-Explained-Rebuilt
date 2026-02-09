40a476c4b14a26e7b99b50a73d1858b06c3fbba7
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-02-09 17:27:21 +0530

    Update layer_norm.py

--- papers/01_Attention_Is_All_You_Need/layer_norm.py
+++ papers/01_Attention_Is_All_You_Need/layer_norm.py
@@ -1,8 +1,7 @@
 """
 Layer Normalization (used in Transformer blocks).
 
-This implementation normalizes across feature dimensions
-and includes learnable scale (gamma) and shift (beta).
+
 """
 
 import numpy as np
