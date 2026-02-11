aa4c07dad2212a1b6dea3cd6076791f355e07c14
Krithika Ganesan <113279833+krithikag23@users.noreply.github.com>
2026-02-11 13:53:49 +0530

    Update encoder_layer.py

--- papers/01_Attention_Is_All_You_Need/encoder_layer.py
+++ papers/01_Attention_Is_All_You_Need/encoder_layer.py
@@ -2,3 +2,7 @@
 Transformer Encoder Layer (Attention + FFN + Residual + LayerNorm).
 """
 
+import numpy as np
+from multi_head_attention import MultiHeadAttention
+from layer_norm import LayerNorm
+from feed_forward import FeedForward
